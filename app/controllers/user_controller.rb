e3f19aaf0be3724a32842256af97792973352e15
zahid208 <zahidhanif208.com>
2018-02-19 17:31:22 +0500

    first commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/update'
+
+  get 'user/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
